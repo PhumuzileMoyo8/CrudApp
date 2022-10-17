1071970f85d4410d68ef1ff4fcd7cd7e243d260d
PhumuzileMoyo8 <Phumuzilemoyo18@gmail.com>
2022-10-17 18:01:40 -0400

    crud app for class demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const users = [{ name: "Phumuzile", username: "Phumz" },
+{ name: "Mallory", username: "Mal" },
+{ name: "Kyle", username: "Kyle" },
+{ name: "Jackson", username: "Jackson" },
+{ name: "Usaid", username: "Usaid" },
+];
+
 function App() {
+  const [Name, setName] = useState('');
+  const [userName, setUserName] = useState('');
+  const [userList, setUserList] = useState(users);
+
+  const onSubmit = () => {
+    setUserList([...users, {name: Name, username: userName},]);
+    setName('');
+    setUserName('');
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='input'>
+      <div className='form'>
+        <label>Name: </label>
+        <input type="text" name="name" onChange={(e) => {setName(e.target.value)}}/>
+        <label>UserName: </label>
+        <input type="text" name="username" onChange={(e) => {setUserName(e.target.value)}}/>
+        <button onClick={onSubmit}>Submit</button>
+
+        {userList.map((key) => {
+            return (<div className='users'>
+              <h3>{key.name}</h3>
+              <p>
+              UserName: {key.username}
+              </p>
+             </div>
+            )
+        }
+        )}
+        </div>
     </div>
   );
 }
